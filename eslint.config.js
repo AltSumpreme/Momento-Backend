6536a05b4392d60e292a9039b75f61eee7a129c0
AltSumpreme <reubenphilip52009@gmail.com>
2025-01-12 12:05:07 +0530

    feat:husky added to the project

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,50 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  {
+    ignores: [
+      "node_modules",
+      ".eslintignore",
+      ".gitignore",
+      "pre-commit",
+      ".prettierignore",
+      ".env",
+      "prisma/",
+    ],
+    files: ["**/*.{js,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: globals.browser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      "prettier/prettier": [
+        "error",
+        {
+          semi: true,
+          singleQuote: false,
+          trailingComma: "es5",
+          printWidth: 80,
+          tabWidth: 2,
+        },
+      ],
+    },
+  },
 ];
