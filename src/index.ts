c58535380039a42bc0718d0aa5802ad974878c53
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-01-15 00:38:47 +0530

    feat: implement v1 auth routes and user model with Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { authRouter } from "./routes/auth.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => c.text("Hello Hono!"));
+app.route("/auth", authRouter);
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
   fetch: app.fetch,
-  port
-})
+  port,
+});
---
+++ src/routes/auth.ts
@@ -0,0 +1,74 @@
+import { Hono } from "hono";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { PrismaClient, Role } from "@prisma/client";
+
+const authRouter = new Hono();
+const JWT_SECRET: string = process.env.JWT_SECRET || "";
+const prisma = new PrismaClient();
+
+authRouter.post("/signup", async (c) => {
+  const { name, email, password } = await c.req.json();
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  try {
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        role: Role.USER,
+      },
+    });
+
+    return c.json({ message: "Signup successful" });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({ error: error.message }, 400);
+    }
+    return c.json({ error: "An unknown error occurred" }, 400);
+  }
+});
+
+authRouter.post("/login", async (c) => {
+  const { email, password }: { email: string; password: string } =
+    await c.req.json();
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      return c.json({ error: "Invalid email or password" }, 400);
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return c.json({ error: "Invalid email or password" }, 400);
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email, role: user.role },
+      JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return c.json({ message: "Login successful", token });
+  } catch (error) {
+    if (error instanceof Error) {
+      return c.json({ error: error.message }, 400);
+    }
+    return c.json({ error: "An unknown error occurred" }, 400);
+  }
+});
+
+authRouter.post("/logout", (c) => {
+  // Endpoint for the sake of it. Client side should remove the token from local storage or cookies
+  return c.json({ message: "Logout successful" });
+});
+
+export { authRouter };
